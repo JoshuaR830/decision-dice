258ff3103dabfe1fd6e525e3f72ae4098e3521f1
Joshua <joshuaquarryhouse@gmail.com>
2023-08-17 10:12:40 +0100

    Adding Lambda

--- src/MotivatorManagementFunction/Program.cs
+++ src/MotivatorManagementFunction/Program.cs
@@ -1,4 +1,17 @@
+<<<<<<< HEAD
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplicationServices();
 builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
 builder.ConfigureApplication();
+=======
+//var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddApplicationServices();
+//builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+
+//var app = builder.Build();
+
+//app.UseSwagger();
+//app.UseSwaggerUI();
+
+//app.MapGet
+>>>>>>> 681aa09 (Adding Lambda)
