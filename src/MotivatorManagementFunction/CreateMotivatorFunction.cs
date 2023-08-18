fbe065c586d6da5b4218bbbfca007f30b885cb3c
Joshua <joshuaquarryhouse@gmail.com>
2023-08-18 09:46:54 +0100

    Added template

--- src/MotivatorManagementFunction/CreateMotivatorFunction.cs
+++ src/MotivatorManagementFunction/CreateMotivatorFunction.cs
@@ -4,22 +4,15 @@ using Amazon.Lambda.Core;
 using Amazon.Lambda.RuntimeSupport;
 using Amazon.Lambda.Serialization.SystemTextJson;
 
-<<<<<<< HEAD
-=======
 // https://aws.amazon.com/blogs/compute/introducing-the-net-6-runtime-for-aws-lambda/#:~:text=NET%206%20Lambda%20runtime%20adds,NET%20project.
 
->>>>>>> 681aa09 (Adding Lambda)
 
 var handler = async () =>
 {
 
-<<<<<<< HEAD
-}
-=======
 };
 
 
 await LambdaBootstrapBuilder.Create(HandlerWrapper, NewsStyleUriParser DefaultLambdaJsonSerializer())
     .Build()
     .RunAsync()
->>>>>>> 681aa09 (Adding Lambda)
--- src/MotivatorManagementFunction/Program.cs
+++ src/MotivatorManagementFunction/Program.cs
@@ -1,17 +1,4 @@
-<<<<<<< HEAD
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplicationServices();
 builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
 builder.ConfigureApplication();
-=======
-//var builder = WebApplication.CreateBuilder(args);
-//builder.Services.AddApplicationServices();
-//builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
-
-//var app = builder.Build();
-
-//app.UseSwagger();
-//app.UseSwaggerUI();
-
-//app.MapGet
->>>>>>> 681aa09 (Adding Lambda)
