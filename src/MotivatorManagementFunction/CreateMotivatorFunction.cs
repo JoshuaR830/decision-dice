512590ef233282c3c5dcfef5f3afc2ba16d37870
Joshua <joshua.richardson@myunidays.com>
2023-08-18 10:15:27 +0100

    Moved function code to Program

--- src/MotivatorManagementFunction/CreateMotivatorFunction.cs
+++ src/MotivatorManagementFunction/CreateMotivatorFunction.cs
@@ -1,35 +1,35 @@
-﻿using System.Net;
+﻿//using System.Net;
 
-using Amazon.Lambda.Core;
-using Amazon.Lambda.RuntimeSupport;
-using Amazon.Lambda.Serialization.SystemTextJson;
-using Amazon.S3;
-using Amazon.Lambda.S3Events;
+//using Amazon.Lambda.Core;
+//using Amazon.Lambda.RuntimeSupport;
+//using Amazon.Lambda.Serialization.SystemTextJson;
+//using Amazon.S3;
+//using Amazon.Lambda.S3Events;
 
-using decision_dice.Models;
-using System.Text.Json.Serialization;
+//using decision_dice.Models;
+//using System.Text.Json.Serialization;
 
-//[assembly: LambdaSerializer(typeof(SourceGeneratorLambdaJsonSerializer<HttpApiJsonSerializerContext>))]
+////[assembly: LambdaSerializer(typeof(SourceGeneratorLambdaJsonSerializer<HttpApiJsonSerializerContext>))]
 
-namespace Management;
+//namespace Management;
 
-var s3Client = new AmazonS3Client();
-// Need to add the types that will be serialized for faster responses
-//[JsonSerializable(typeof(Motivator))]
-//public partial class HttpApiJsonSerializerContext : JsonSerializerContext { }
+//var s3Client = new AmazonS3Client();
+//// Need to add the types that will be serialized for faster responses
+////[JsonSerializable(typeof(Motivator))]
+////public partial class HttpApiJsonSerializerContext : JsonSerializerContext { }
 
-// https://aws.amazon.com/blogs/compute/introducing-the-net-6-runtime-for-aws-lambda/#:~:text=NET%206%20Lambda%20runtime%20adds,NET%20project.
+//// https://aws.amazon.com/blogs/compute/introducing-the-net-6-runtime-for-aws-lambda/#:~:text=NET%206%20Lambda%20runtime%20adds,NET%20project.
 
-var handler = async (S3Event evnt, ILambdaContext context) =>
-{
-    foreach(var record in evnt.Records)
-    {
-        using var response = await s3Client.GetObjectAsync(record.S3.Bucket.Name, record.S3.Object.Key);
-        using ver reader = new StreamReader(response.ResponseStream);
-    }
-};
+//var handler = async (S3Event evnt, ILambdaContext context) =>
+//{
+//    foreach(var record in evnt.Records)
+//    {
+//        using var response = await s3Client.GetObjectAsync(record.S3.Bucket.Name, record.S3.Object.Key);
+//        using ver reader = new StreamReader(response.ResponseStream);
+//    }
+//};
 
-await LambdaBootstrapBuilder.Create(handler, new DefaultLambdaJsonSerializer())
-    .Build()
-    .RunAsync();
+//await LambdaBootstrapBuilder.Create(handler, new DefaultLambdaJsonSerializer())
+//    .Build()
+//    .RunAsync();
 
--- src/MotivatorManagementFunction/Program.cs
+++ src/MotivatorManagementFunction/Program.cs
@@ -1,4 +1,25 @@
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.RuntimeSupport;
+using Amazon.Lambda.Serialization.SystemTextJson;
+using Amazon.S3;
+using decision_dice.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplicationServices();
 builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
 builder.ConfigureApplication();
+
+var s3Client = new AmazonS3Client();
+
+// A URL lambda still takes APIGatewayHttpApiV2ProxyRequest
+var handler = async (APIGatewayHttpApiV2ProxyRequest input, ILambdaContext context) =>
+{
+    var motivator = JsonSerializer.Deserialize<Motivator>(input.Body);
+
+    return $"New motivator for {motivator.UserName}, callled {motivator.Title}";
+};
+
+await LambdaBootstrapBuilder.Create(handler, new DefaultLambdaJsonSerializer())
+    .Build()
+    .RunAsync();
